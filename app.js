5d25aa6ef3a240cb492b04ca0288b7bd4626fe19
Per-Olov Jernberg <possan@possan.se>
2013-05-25 17:52:50 +0200

    Post emotion data to backend

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var fs = require('fs');
+var http = require('http');
+var querystring = require('querystring');
+
 var Tail = require('tail').Tail;
 var tail = new Tail("/var/log/keystroke.log");
 var historylength = 20;
@@ -30,6 +33,34 @@ tail.on("line", function(data) {
 				match = false;
 		if (match) {
 			console.log('Log emotion:', matcher.scoring);
+			for(var k in matcher.scoring) {
+				var v = matcher.scoring[k];
+
+				console.log('Log key', k, 'value', v);
+
+				var data = querystring.stringify({key: k, value: v});
+
+				var options = {
+				    host: '192.168.200.37',
+				    port: 8089,
+				    path: '/emo',
+				    method: 'POST',
+				    headers: {
+				        'Content-Type': 'application/x-www-form-urlencoded',
+				        'Content-Length': data.length
+				    }
+				};
+
+				var req = http.request(options, function(res) {
+				    res.setEncoding('utf8');
+				    res.on('data', function (chunk) {
+				        console.log("body: " + chunk);
+				    });
+				});
+
+				req.write(data);
+				req.end();
+			}
 		}
 	});
 
